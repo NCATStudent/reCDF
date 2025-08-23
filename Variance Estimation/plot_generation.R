db6cd4bced8d2891d9e20bd0bb4f079b1327b812
Jeremy Flood <jrfloodusc@gmail.com>
2025-08-23 16:49:43 -0400

    finalize results

--- Variance Estimation/plot_generation.R
+++ Variance Estimation/plot_generation.R
@@ -4,15 +4,15 @@ setwd("/Users/jeremyflood/Library/CloudStorage/OneDrive-Personal/Documents/Grad 
 
 cleaned_results <-
   openxlsx::read.xlsx("final_agg_results.xlsx", sheet = "summary") %>%
-  bind_rows() #%>% #what if we kept the (previous) bootstrap t?
-  # mutate(var_type = ifelse(
-  #   est_type == "t" & var_type == "boot", "boot_ignore", var_type
-  # )) %>%
-  # filter(var_type != "boot_ignore") %>%
-  # mutate(var_type = ifelse(
-  #   var_type == "boot2", "boot", var_type
-  # )) %>%
-  # dplyr::select(-name) # needs to be checked for general case of all data together; my data looks interesting bc for loop failed and needed to depend on cached results
+  bind_rows() %>% 
+  mutate(var_type = ifelse(
+    est_type == "t" & var_type == "boot", "boot_ignore", var_type
+  )) %>%
+  filter(var_type != "boot_ignore") %>%
+  mutate(var_type = ifelse(
+    var_type == "boot2", "boot", var_type
+  )) %>%
+  dplyr::select(-name) # needs to be checked for general case of all data together; my data looks interesting bc for loop failed and needed to depend on cached results
 
 
 plots <- c()
@@ -68,7 +68,7 @@ for (i in 1:length(mods)) {
 TN_plots <- c()
 
 for (i in 1:length(mods)) {
- # TN_plots[[i]] <-
+  TN_plots[[i]] <-
     cleaned_results %>%
     filter(mod == paste0("f", mods[i]), est_type == "t") %>% # for now
     mutate(se_val = sqrt(est_var)) %>%
@@ -94,12 +94,12 @@ for (i in 1:length(mods)) {
       labels = c("1000" = TeX("$n_{B} = n_A$"), "20000" = TeX("$n_{B} = 20n_A$"))
     )) %>%
     mutate(var_name = factor(var_type, 
-                             levels = c('asymp', 'boot2', 'boot'),
-                             labels = c("asymp" = "Asymptotic", "boot" = "Proposed Bootstrap", 'boot2'= 'Standard Bootstrap'))) %>%
+                             levels = c('asymp', 'boot'),
+                             labels = c("asymp" = "Asymptotic", "boot" = " Bootstrap"))) %>%
     ggplot(aes(x = perc, y = value, col = var_name, group = var_name)) +
     geom_line(linewidth = .5) +
     geom_point(size = 1) +
-    scale_color_discrete(labels = unname(TeX(c("Asymptotic", 'Proposed Bootstrap', "Standard Bootstrap")))) +
+    scale_color_discrete(labels = unname(TeX(c("Asymptotic", "Bootstrap")))) +
     facet_grid(
       scales = "free", col = dplyr::vars(miss, nB), rows = dplyr::vars(perf),
       labeller = label_parsed
